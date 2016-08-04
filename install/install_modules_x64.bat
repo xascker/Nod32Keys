b16936eff1247b2e54da62f6c01d78dd2ba0a691
Sergey <sergey@voicebase.com>
2016-08-04 15:28:04 +0300

    Added bat scripts for auto install python modules

---
+++ install/install_modules_x64.bat
@@ -0,0 +1,6 @@
+@echo off
+
+cd C:\Python27\Scripts
+pip install C:\Nod32Keys\install\cssselect-0.9.2-py2.py3-none-any.whl
+pip install C:\Nod32Keys\install\lxml-3.6.1-cp27-cp27m-win_amd64.whl
+pip install C:\Nod32Keys\install\requests-2.2.1-py2.py3-none-any.whl
